e9d1e62795f6b875facef669516d4a42616ce090
joseamirandavelez <joseamirandavelez@gmail.com>
2017-07-26 00:10:19 -0400

    Using ESP-01 without libraries
    Changed the WiFi module to an ESP-01 (ESP8266)

--- Arduino101_Sensors.ino
+++ Arduino101_Sensors.ino
@@ -2,29 +2,24 @@
 //Load Includes
 //*****************
 #include <DHT.h>
-//#include <CurieIMU.h>
-#include <ELClient.h>
-#include <ELClientRest.h>
+#include <CurieIMU.h>
 #include <neotimer.h>
 
-#define ESPBootTime 10
+// Access Point Info
+String ssid ="Miranda_Net_Arris";
+String password="cafeconleche";
 
-ELClient esp(&Serial, &Serial);
+// Api information
+String token = "58156522556c68d0963a3906b7f0c033";
+String apiServer ="api.nubeiot.com";
 
 // General variables
-const int serialSpeed = 9600;
-
-ELClientRest rest(&esp);
-boolean wifiConnected = false;
-#define BUFLEN 266                                    // Data buffer
-#define api_key "eaaeebace112754929d81074576e9e1b"    // NubeIoT API Key
-
-//Optoisolator pin
-#define optoPin 7
-
+const int serialSpeed = 115200;
+boolean connected = false;
+#define CONNECT_ATTEMPTS 10       // Atemps to connect to AP
 
 // Temperature sensor initialization
-#define DHTPIN 2        // what digital pin we're connected to
+#define DHTPIN 3        // what digital pin we're connected to
 #define DHTTYPE DHT11   // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -44,177 +39,133 @@ float lastTime=0;
 // Timers initializastion
 Neotimer dhtTimer = Neotimer(500);
 
+// Timers initializastion
+Neotimer apiTimer = Neotimer(60000);
+
 // Initial Setup
 void setup() {
-  Serial.begin(serialSpeed);    // USB serial for debugging messages
-
-  /*
-  #ifdef SOFTSERIAL
-    ESPSerial.begin(serialSpeed);
-  #endif
-  */
-  //#ifdef HARDSERIAL
-    //Serial1.begin(serialSpeed);   // Hardware serial for communication with ESP8266 (Arduino 101 uses Serial1)
-  //#endif 
+  Serial.begin(9600);    // USB serial for debugging messages
+  Serial1.begin(serialSpeed);   // Serial1 (hardware serial) on Arduino 101
+ 
+  Serial.print("Loading...");
+  Serial1.println("AT+RST");
+  delay(1000);
   
-  Serial.println("Waiting for ESP to boot...");
-  for(int x = ESPBootTime; x > 0; x--){
-    Serial.print(x);
-    Serial.println("...");
-    delay(1000);
-  }
-  
-  //Set optocoupler pint to output and send HIGH signal
-  pinMode(optoPin, OUTPUT);     
-  digitalWrite(optoPin, HIGH);  // Set optoisolator pin to high to enable serial transmission
-  Serial.println("Serial communication enabled...");
-
-  // Start wifi connection code //
-  Serial.println("");
-  Serial.println("EL-Client starting!");
-
-  esp.wifiCb.attach(wifiCb); // wifi status change callback, optional (delete if not desired)
-  bool ok;
-  do {
-    ok = esp.Sync();      // sync up with esp-link, blocks for up to 2 seconds
-    if (!ok) {
-      Serial.println("");
-      Serial.println("EL-Client sync failed! err: ");
-      Serial.println(ok);
-    }
-  } while(!ok);
-  Serial.println("EL-Client synced!");
-
-  // Wait for WiFi to be connected.
-  esp.GetWifiStatus();
-  ELClientPacket *packet;
-  Serial.println("Waiting for WiFi ");
-  if ((packet=esp.WaitReturn()) != NULL) {
+  boolean connected = false;
+  // try to establish a connection
+  Serial.print("Connecting to Wifi...");
+  for (int i = 0; i < CONNECT_ATTEMPTS; i++) {
     Serial.print(".");
-    Serial.println(packet->value);
+    if (wifiConnect()) {
+      //Serial.println("");
+      connected = true;
+      Serial.println("\nConnected to:" + ssid);
+      break;
+    }
   }
-  Serial.println("");
-
-  // Set up the REST client to talk to nubeIoT, this doesn't connect to that server,
-  // it just sets-up stuff on the esp-link side
-  int err = rest.begin("api.nubeiot.com");
-  if (err != 0) {
-    Serial.print("REST begin failed: ");
-    Serial.println(err);
-    while(1) ;
+  if(connected == true){
+    // Count for delay get ip dhcp bla bla bla...
+    Serial.print("Adquiring DHCP");
+    for(int i=0; i < 5; i ++){
+      Serial.print(".");
+      delay(1000);
+    }
+  } else {
+    Serial.println("Unable to Connect after 10 tries. Go figure... :(");
   }
-  Serial.println("ESP-Link REST ready!");
   // End wifi connection code //
   
   // Setup IMU
   Serial.println("Initializing IMU device...");
-  //CurieIMU.begin();
-  // Set the accelerometer range to 2G
-  //CurieIMU.setAccelerometerRange(4);
+  CurieIMU.begin();
+  CurieIMU.setAccelerometerRange(4);    // Set the accelerometer range to 4G
 
   // Setup DHT11
   Serial.println("Initializing DHT11 sensor...");
   dht.begin();
   dhtTimer.start();
-}
 
-// Callback made from esp-link to notify of wifi status changes
-// Here we print something out and set a global flag
-void wifiCb(void *response) {
-  ELClientResponse *res = (ELClientResponse*)response;
-  if (res->argc() == 1) {
-    uint8_t status;
-    res->popArg(&status, 1);
-
-    if(status == STATION_GOT_IP) {
-      Serial.println("WIFI CONNECTED");
-      wifiConnected = true;
-    } else {
-      Serial.print("WIFI NOT READY: ");
-      Serial.println(status);
-      wifiConnected = false;
-    }
-  }
+  apiTimer.start();
 }
 
 //*****************
 //Main Program Loop
 //*****************
-void loop() {
-    //Get Time
-    lastTime=millis();
+void loop() {  
+  //Get Time
+  lastTime=millis();
 
-    //This is done twice per second
-    if(dhtTimer.done()) {
-      // Read sensors
-      lastHumid = dht.readHumidity();
-      lastTemp = dht.readTemperature(true);
-      lastGasQ = analogRead(GAS_PIN);
-      
-      // Timer reset
-      dhtTimer.reset();
-      dhtTimer.start();
-    }
-    // Read IMU
-    //CurieIMU.readAccelerometerScaled(ax, ay, az);
-    //lastAccel=az;
-  
-    //Serial.print (lastTime);
-    //Serial.print (" ");
-    Serial.print (lastTemp);
-    Serial.print (" ");
-    Serial.print (lastHumid);
-    Serial.print (" ");
-    Serial.print (lastAccel);
-    Serial.print (" ");
-    Serial.println (lastGasQ);
-
-  //Send data to NubeIoT
-  // process any callbacks coming from esp_link
-  esp.Process();
-
-  // if we're connected make an REST request
-  if(wifiConnected) {
-    // Reserve a buffer for sending the data
-    char path_data[BUFLEN];
-    // Copy the path and API key into the buffer
-    sprintf(path_data, "%s", "/?token=");
-    sprintf(path_data + strlen(path_data), "%s", api_key);
+  //This is done twice per second
+  if(dhtTimer.done()) {
+    // Read sensors
+    lastHumid = dht.readHumidity();
+    lastTemp = dht.readTemperature(true);
+    lastGasQ = analogRead(GAS_PIN);
     
-    // Copy the field number and value into the buffer
-    // If you have more than one field to update,
-    // repeat and change field1 to field2, field3, ...
-
-    sprintf(path_data + strlen(path_data), "%s", "&Temperature=");
-    sprintf(path_data + strlen(path_data), "%f", lastTemp);
-    sprintf(path_data + strlen(path_data), "%s", "&Humidity=");
-    sprintf(path_data + strlen(path_data), "%f", lastHumid);
-    sprintf(path_data + strlen(path_data), "%s", "&gasQuality=");
-    sprintf(path_data + strlen(path_data), "%f", lastGasQ);
-    sprintf(path_data + strlen(path_data), "%s", "&Acceleration=");
-    sprintf(path_data + strlen(path_data), "%f", lastAccel);
-
-    
-    // Send PUT request to nubeiot.com
-    rest.post(path_data,"");
-
-    // Reserve a buffer for the response from Thingspeak
-    char response[BUFLEN];
-    // Clear the buffer
-    memset(response, 0, BUFLEN);
-    // Wait for response from Thingspeak
-    uint16_t code = rest.waitResponse(response, BUFLEN-1);
-    // Check the response from Thingspeak
-    if(code == HTTP_STATUS_OK){
-      Serial.println("POST successful:");
-      Serial.print("Response: ");
-      Serial.println(response);
-    } else {
-      Serial.print("POST failed with error ");
-      Serial.println(code);
-      Serial.print("Response: ");
-      Serial.println(response);
-    }
-    
+    // Timer reset
+    dhtTimer.reset();
+    dhtTimer.start();
   }
+  // Read IMU
+  CurieIMU.readAccelerometerScaled(ax, ay, az);
+  lastAccel=az;
+
+  if(apiTimer.done()) {
+    // Send data to NubeIoT
+    boolean answer = sendData();
+    
+    // Timer reset
+    apiTimer.reset();
+    apiTimer.start();
+  }
+
+  //Serial.print (lastTime);
+  //Serial.print (" ");
+  Serial.print (lastTemp);
+  Serial.print (" ");
+  Serial.print (lastHumid);
+  Serial.print (" ");
+  Serial.print (lastAccel);
+  Serial.print (" ");
+  Serial.println (lastGasQ);
+}
+
+boolean wifiConnect(){
+  Serial1.println("AT+CWMODE=1");
+  Serial1.println("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"" );
+ 
+  delay(2000);
+  if (Serial1.find("OK")) {
+  return true;
+  } else {
+  return false;
+  }
+}
+
+boolean sendData(){
+  String path_data;
+  path_data = "GET /?token=" + token;
+  path_data = path_data + "&Temperature=" + lastTemp;
+  path_data = path_data + "&Humidity=" + lastHumid;
+  path_data = path_data + "&gasQuality=" + lastGasQ;
+  //path_data = path_data + "&Acceleration=" + lastAccel;
+  path_data = path_data + " HTTP/1.0\r\nHOST: api.nubeiot.com" + "\r\nConnection: close\r\n\r\n";
+  
+  String cmd = "AT+CIPSTART=\"TCP\",\"" + apiServer +"\",80";
+  //String getCmd = "GET /?token=" + token + "&test=999 HTTP/1.0\r\nHost: " + apiServer +"\r\nConnection: close\r\n\r\n" ;
+  Serial.println(cmd);
+  Serial1.println(cmd);
+  delay(500);
+  Serial.print("AT+CIPSEND=");
+  Serial.println( path_data.length() );
+  Serial1.print("AT+CIPSEND=");
+  Serial1.println( path_data.length() );
+ 
+  delay(1000);
+  Serial1.println(path_data);
+  Serial.println(path_data);
+  Serial1.println("AT+CIPCLOSE");
+  Serial.println("AT+CIPCLOSE");
+  
+  return true;
 }
